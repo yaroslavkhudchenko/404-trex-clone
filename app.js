56f820acf0ff6910df8650df3890bb731c365ba2
YariPL <yari.lune@gmail.com>
2020-06-09 13:02:20 +0200

    set good camera position

--- app.js
+++ app.js
@@ -12,14 +12,16 @@ const init = () => {
 
     
     camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 100);
-    camera.position.set(0,5,-10);
+    camera.position.set(5.962310066149917, 5.396010288886008, -9.42343565435011);
+
+    /* set({x:-2.68,y:0.666,z:2.84}); */
     scene = new THREE.Scene();
 
     light = new THREE.DirectionalLight(0xffe57c, 1);
     // light = new THREE.AmbientLight(0xffffff,1);
     scene.add(light);
 
-    geometry = new THREE.BoxGeometry(11, 1, 4);
+    geometry = new THREE.BoxGeometry(112, 1, 4);
     material = new THREE.MeshPhongMaterial({
         color: 0x656565,
         specular: 0x000000,
@@ -46,6 +48,7 @@ const animate = () => {
     requestAnimationFrame(animate);
     controls.update();
     renderer.render(scene, camera);
+    console.log(camera.position);
 }
 
 init();
