a67bf2b01c20ffc782ab0b1e90bfd06b126866a7
YariPL <yari.lune@gmail.com>
2020-06-10 11:33:36 +0200

    comment code

--- app.js
+++ app.js
@@ -16,29 +16,33 @@ const init = () => {
 
     
     camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 100);
+
+    // set camera position(look from right side)
     camera.position.set(
         5.189239552338781,
         3.7861393344249135,
         -9.815769086484597
     );
+    
+    // create scene
     scene = new THREE.Scene();
 
-
-    // axis helper
+    // axis helper(to see axis visully)
     let axesHelper = new THREE.AxesHelper(9);
     scene.add(axesHelper);
-
+    
+    // ambient light for scene
+    let ambient = new THREE.AmbientLight(0xffffff, 1);
+    // light(like sun)
     light = new THREE.DirectionalLight(0xffe57c, 1);
-    // light = new THREE.AmbientLight(0xffffff,1);
     scene.add(light);
-
+    scene.add(ambient)
     // floor
     geometryFloor = new THREE.BoxGeometry(111, 0, 7);
     materialFloor = new THREE.MeshPhongMaterial({
         color: 0x656565,
         specular: 0x000000,
-        shininess: 100/* ,
-        envMaps:refraction */
+        shininess: 100
     });
     floorMesh = new THREE.Mesh(geometryFloor, materialFloor);
     scene.add(floorMesh);
@@ -50,6 +54,8 @@ const init = () => {
     playerMesh.position.set(playerDefaultPosition.x, playerDefaultPosition.y, playerDefaultPosition.z);
     scene.add(playerMesh);
 
+
+    // spawn enemies every (between 3 and .5 seconds)
     setInterval(() => enemySpawner(), Math.floor((Math.random() * 3000) + 500));
 
 
@@ -57,14 +63,15 @@ const init = () => {
         antialias: true,
         canvas: canvas
     });
-    renderer.setClearColor(0xfff000);
+    renderer.setClearColor(0xfff000); // to have light background color
     renderer.setSize(window.innerWidth, window.innerHeight);
 
-    // create the controls
+    // create the controls(for testing)
     controls = new OrbitControls(camera, canvas);
     controls.update();
 
 
+    // pointer to see where enemies eliminates
     let pointerGeo = new THREE.CubeGeometry(2, 2, 2);
     let pointerMat = new THREE.MeshBasicMaterial({ color: 0x0000f0 })
     let pointer = new THREE.Mesh(pointerGeo, pointerMat);
@@ -75,33 +82,31 @@ const init = () => {
 
 }
 
+// to spawn enemies
 const enemySpawner = () => {
-    console.log('enemyspawner')
+
+    console.log('enemy spawner')
+
+    // enemy mesh
     let enemyG = new THREE.CubeGeometry(1, 1, 1);
     let enemyMat = new THREE.MeshBasicMaterial({ color: 0x00f000 })
     let enemy = new THREE.Mesh(enemyG, enemyMat);
+    
+    // set position
     enemy.position.set(Math.floor((Math.random() * -25) + -25), Math.floor((Math.random() * 2) + 1), 0);
     enemy.name = 'enemy';
     scene.add(enemy);
 
+    // to move
     setInterval(() => {
         enemy.position.x += .1;
     }, Math.floor((Math.random() * .5) + 1));
 
-    enemies.push(enemy);
-    //console.log(enemies);
-    /* if(enemies.length > 2) {
-        console.log('more')
-        scene.remove(enemies[0])
-        scene.remove(enemies[1])
-    }
- */
-
-   
+    enemies.push(enemy); // push to global array to control if reach the pointer
 
 }
 
-
+// handle keypress/ up function to interact with the player obj
 const keyPressedHandler = (e) => {
     switch (e.code) {
         case "KeyS":
@@ -126,12 +131,13 @@ const keyUpHandler = (e) => {
 }
 
    
-
+// main animate function
 const animate = () => {
     requestAnimationFrame(animate);
     controls.update();
     renderer.render(scene, camera);
-    // console.log(camera.position);
+
+    // check if any of the enemies reach the destroyer pointer and if yes remove from the scene
     enemies.map((e) => {
         e.position.x > 25 ? 
             scene.remove(e) :
@@ -143,5 +149,6 @@ const animate = () => {
 init();
 animate();
 
+// events
 document.addEventListener('keypress', keyPressedHandler);
 document.addEventListener('keyup', keyUpHandler);
