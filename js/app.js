570a75c0663870e91ebde23b1aafed021b68f5e1
YariPL <yari.lune@gmail.com>
2020-06-10 11:44:45 +0200

    create separate file for enemies

--- js/app.js
+++ js/app.js
@@ -5,13 +5,14 @@ import * as THREE from '../three.module.js';
 import { OrbitControls } from '../OrbitControls.js';
 
 import { player, playerMesh, playerDefaultPosition } from './player.js';
+import { enemySpawner } from './enemies.js';
 
 export let camera, scene, renderer, controls;
 export let geometryFloor, materialFloor, floorMesh, light;
 export let canvas = document.querySelector('#gameCanvas');
 
 
-let enemies = [];
+export let enemies = [];
 
 
 const init = () => {
@@ -79,29 +80,6 @@ const init = () => {
 
 }
 
-// to spawn enemies
-const enemySpawner = () => {
-
-    console.log('enemy spawner')
-
-    // enemy mesh
-    let enemyG = new THREE.CubeGeometry(1, 1, 1);
-    let enemyMat = new THREE.MeshBasicMaterial({ color: 0x00f000 })
-    let enemy = new THREE.Mesh(enemyG, enemyMat);
-    
-    // set position
-    enemy.position.set(Math.floor((Math.random() * -25) + -25), Math.floor((Math.random() * 2) + 1), 0);
-    enemy.name = 'enemy';
-    scene.add(enemy);
-
-    // to move
-    setInterval(() => {
-        enemy.position.x += .1;
-    }, Math.floor((Math.random() * .5) + 1));
-
-    enemies.push(enemy); // push to global array to control if reach the pointer
-
-}
 
 // handle keypress/ up function to interact with the player obj
 const keyPressedHandler = (e) => {
---
+++ js/enemies.js
@@ -0,0 +1,26 @@
+import * as THREE from '../three.module.js';
+import { scene, enemies } from './app.js';
+
+// to spawn enemies
+export const enemySpawner = () => {
+
+    console.log('enemy spawner')
+
+    // enemy mesh
+    let enemyG = new THREE.CubeGeometry(1, 1, 1);
+    let enemyMat = new THREE.MeshBasicMaterial({ color: 0x00f000 })
+    let enemy = new THREE.Mesh(enemyG, enemyMat);
+
+    // set position
+    enemy.position.set(Math.floor((Math.random() * -25) + -25), Math.floor((Math.random() * 2) + 1), 0);
+    enemy.name = 'enemy';
+    scene.add(enemy);
+
+    // to move
+    setInterval(() => {
+        enemy.position.x += .1;
+    }, Math.floor((Math.random() * .5) + 1));
+
+    enemies.push(enemy); // push to global array to control if reach the pointer
+
+}
