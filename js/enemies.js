d79055307fb6f715e7a8b7f845e24b5e00c2a24d
YariPL <yari.lune@gmail.com>
2020-07-07 13:29:08 +0200

    rebuild ptero structure in enemies for 3 -> change to object

--- js/enemies.js
+++ js/enemies.js
@@ -25,17 +25,41 @@ export const enemySpawner = () => {
 }
 export const enemyPteroSpawner = () => {
 
-    // enemy mesh
-    let enemy = enemyObjTopOne.clone(); // new THREE.Mesh(enemyG, enemyMat);
-    enemy.name = 'enemyPtero';
-    enemy.scale.set(0.2, 0.2, 0.2)
-    enemy.rotation.set(0, 0, 0)
+    // ptero mesh
+    let enemy = { 
+        one: enemyObjTopOne.clone(),
+        two: enemyObjTopOne.clone(),
+        three: enemyObjTopOne.clone()
+    }
+    enemy.one.name = 'enemyPtero';
+    enemy.two.name = 'enemyPtero';
+    enemy.three.name = 'enemyPtero';
+
+    enemy.one.scale.set(0.2, 0.2, 0.2)
+    enemy.two.scale.set(0.2, 0.2, 0.2)
+    enemy.three.scale.set(0.2, 0.2, 0.2)
+
+    enemy.one.rotation.set(0, 0, 0)
+    enemy.two.rotation.set(0, 0, 0)
+    enemy.three.rotation.set(0, 0, 0)
 
     // set position
-    enemy.position.set(-100, 9, 0);
-    enemy.castShadow = true; //default is false
-    enemy.receiveShadow = false;
-    scene.add(enemy);
+    enemy.one.position.set(-100, 9, 0);
+    enemy.two.position.set(-100, 9, 0);
+    enemy.three.position.set(-100, 9, 0);
+
+    enemy.one.castShadow = true; //default is false
+    enemy.two.castShadow = true; //default is false
+    enemy.three.castShadow = true; //default is false
+
+    enemy.one.receiveShadow = false;
+    enemy.two.receiveShadow = false;
+    enemy.three.receiveShadow = false;
+
+    scene.add(enemy.one);
+    scene.add(enemy.two);
+    scene.add(enemy.three);
+
 
     enemiesPtero.unshift(enemy); // unshift to global array to control if reach the pointer
 }
